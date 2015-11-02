731f4cb3a6613d39609c5cefebc9bb79ba69ebdb
oleyang <oleyang@gmail.com>
2015-11-02 10:38:57 +0800

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Dependencies
 
-go get -u github.com/Terry-Mao/gopush-cluster
-go get -u github.com/Terry-Mao/goconf
-go get -u github.com/garyburd/redigo/redis
+go get -u github.com/oleyang/gopush-cluster
+go get -u github.com/oleyang/goconf
+go get -u github.com/oleyang/redigo/redis
 go get -u code.google.com/p/go.net/websocket
 go get -u github.com/samuel/go-zookeeper
 go get -u code.google.com/p/log4go
